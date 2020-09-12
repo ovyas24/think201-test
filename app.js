133ead0364788a5ebc8bf16e7580e5eb69f8fb2c
ovyas24 <ovyas24@gmail.com>
2020-09-12 19:29:28 +0530

    App development started

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const ejs = require("ejs")
+const mongoose = require("mongoose")
+
+const app = express();
+const port = process.env.port || 3000
+
+//connect database
+
+//init
+app.set("view-engine","ejs")
+app.use(express.urlencoded({extended:true}))
+
+//routes
+app.use("/",require("./routes/index"))
+
+//listening to port
+app.listen(port,(req,res)=>{
+    console.log(`App running on port ${port}`);
+})
